ef59d5e054ed9ea93c93a8cf1573c27f79c93a13
Bradley Bell <bellcha@gmail.com>
2022-08-02 20:22:14 -0500

    culling uneeded data

--- mmbk_eff_ratio_hist.py
+++ mmbk_eff_ratio_hist.py
@@ -8,20 +8,18 @@ class MMBKData:
     REPDTE: str
     ASSET: str
     EEFFQR: str
-    CERT: str
-    ID: str
 
-url = 'https://banks.data.fdic.gov/api/financials?filters=CERT%3A12203&fields=CERT%2CREPDTE%2CASSET%2CEEFFQR&sort_by=REPDTE&sort_order=DESC&limit=10000&offset=0&agg_limit=1&format=json&download=false&filename=data_file'
+url = 'https://banks.data.fdic.gov/api/financials?filters=CERT%3A12203&fields=REPDTE%2CASSET%2CEEFFQR&sort_by=REPDTE&sort_order=DESC&limit=10000&offset=0&agg_limit=1&format=json&download=false&filename=data_file'
 
 data = rq.get(url).text
 
 json_data = json.loads(data)
 
-mmbk_df = pd.DataFrame(columns=['REPDTE','ASSET','EEFFQR','CERT','ID'])
+mmbk_df = pd.DataFrame(columns=['REPDTE','ASSET','EEFFQR'])
 
 
 for d in json_data['data']:
-    results = MMBKData(**d['data'])
+    results = MMBKData(REPDTE=d['data']['REPDTE'], ASSET=d['data']['ASSET'], EEFFQR=d['data']['EEFFQR'])
     row = pd.DataFrame(asdict(results), index=[0])
     mmbk_df = pd.concat([mmbk_df,row], axis=0 ,ignore_index=True)
 
