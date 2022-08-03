6efb3d195b7883cb12204b9d57b063ae2e0d9a08
Bradley Bell <bellcha@gmail.com>
2022-08-02 20:35:08 -0500

    adding main func

--- mmbk_eff_ratio_hist.py
+++ mmbk_eff_ratio_hist.py
@@ -9,20 +9,23 @@ class MMBKData:
     ASSET: int
     EEFFQR: float
 
-url = 'https://banks.data.fdic.gov/api/financials?filters=CERT%3A12203&fields=REPDTE%2CASSET%2CEEFFQR&sort_by=REPDTE&sort_order=DESC&limit=10000&offset=0&agg_limit=1&format=json&download=false&filename=data_file'
+def main():
+    
+    url = 'https://banks.data.fdic.gov/api/financials?filters=CERT%3A12203&fields=REPDTE%2CASSET%2CEEFFQR&sort_by=REPDTE&sort_order=DESC&limit=10000&offset=0&agg_limit=1&format=json&download=false&filename=data_file'
+    
+    data = rq.get(url).text
+    
+    json_data = json.loads(data)
+    
+    mmbk_df = pd.DataFrame(columns=['REPDTE','ASSET','EEFFQR'])
+    
+    
+    for d in json_data['data']:
+        results = MMBKData(REPDTE=d['data']['REPDTE'], ASSET=d['data']['ASSET'], EEFFQR=d['data']['EEFFQR'])
+        row = pd.DataFrame(asdict(results), index=[0])
+        mmbk_df = pd.concat([mmbk_df,row], axis=0 ,ignore_index=True)
+    
+    mmbk_df.to_excel('eff_hist.xlsx', index= False)
 
-data = rq.get(url).text
-
-json_data = json.loads(data)
-
-mmbk_df = pd.DataFrame(columns=['REPDTE','ASSET','EEFFQR'])
-
-
-for d in json_data['data']:
-    results = MMBKData(REPDTE=d['data']['REPDTE'], ASSET=d['data']['ASSET'], EEFFQR=d['data']['EEFFQR'])
-    row = pd.DataFrame(asdict(results), index=[0])
-    mmbk_df = pd.concat([mmbk_df,row], axis=0 ,ignore_index=True)
-
-print(mmbk_df.head(5))
-
-mmbk_df.to_excel('eff_hist.xlsx', index= False)
+if __name__ == '__main__':
+    main()
